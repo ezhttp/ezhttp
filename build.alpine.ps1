4bd7cd4a8250fa53ca9fc82c133d343bf55cce36
Ryan Lelek <ryanlelek@gmail.com>
2025-01-08 21:10:51 -0600

    Edit: Generating binaries for AMD64 x86_64 and ARM64 v8

--- build.alpine.ps1
+++ build.alpine.ps1
@@ -12,13 +12,24 @@ $GitHash = (git log -1 --format=format:"%H")
 # Create ./bin/
 New-Item -ItemType Directory -Force -Path ./bin/ | out-null;
 
-# Build and Copy
-podman build -t "ezhttp:temp" -f Dockerfile-build --ignorefile .dockerignore-build `
+# Build Container Image. We'll copy out the file after
+# AMD64 x86_64
+podman build --platform linux/amd64 -t "ezhttp:temp-amd64" -f Dockerfile-build --ignorefile .dockerignore-build `
     --build-arg build_date="$DateTime" `
     --build-arg build_git_hash="$GitHash" . && `
     podman run --rm `
     -v "./bin:/output" `
     -w /usr/src/app `
-    "ezhttp:temp" `
-    cp "/usr/src/app/ezhttp.alpine.bin" /output/ && `
-    podman image rm "ezhttp:temp";
+    "ezhttp:temp-amd64" `
+    cp "/usr/src/app/ezhttp.alpine.bin" /output/ezhttp.alpine-amd64.bin && `
+    podman image rm "ezhttp:temp-amd64";
+# ARM64 v8
+podman build --platform linux/arm64/v8 -t "ezhttp:temp-arm64v8" -f Dockerfile-build --ignorefile .dockerignore-build `
+    --build-arg build_date="$DateTime" `
+    --build-arg build_git_hash="$GitHash" . && `
+    podman run --rm `
+    -v "./bin:/output" `
+    -w /usr/src/app `
+    "ezhttp:temp-arm64v8" `
+    cp "/usr/src/app/ezhttp.alpine.bin" /output/ezhttp.alpine-arm64v8.bin && `
+    podman image rm "ezhttp:temp-arm64v8";
--- build.alpine.sh
+++ build.alpine.sh
@@ -12,12 +12,23 @@ BUILD_GIT_HASH=$(git rev-list -1 HEAD);
 
 # Build Container Image. We'll copy out the file after
 mkdir -p ./bin/;
-podman build -t "ezhttp:temp" -f Dockerfile-build --ignorefile .dockerignore-build \
+# AMD64 x86_64
+podman build --platform linux/amd64 -t "ezhttp:temp-amd64" -f Dockerfile-build --ignorefile .dockerignore-build \
 	--build-arg build_date="${BUILD_DATE}" \
 	--build-arg build_git_hash="${BUILD_GIT_HASH}" . && \
-podman run --rm \
+	podman run --rm \
 	-v "${PWD}/bin:/output" \
 	-w /usr/src/app \
-	"ezhttp:temp" \
-	cp "/usr/src/app/ezhttp.alpine.bin" /output/ && \
-podman image rm "ezhttp:temp";
+	"ezhttp:temp-amd64" \
+	cp "/usr/src/app/ezhttp.alpine.bin" /output/ezhttp.alpine-amd64.bin && \
+	podman image rm "ezhttp:temp-amd64";
+# ARM64 v8
+podman build --platform linux/arm64/v8 -t "ezhttp:temp-arm64v8" -f Dockerfile-build --ignorefile .dockerignore-build \
+	--build-arg build_date="${BUILD_DATE}" \
+	--build-arg build_git_hash="${BUILD_GIT_HASH}" . && \
+	podman run --rm \
+	-v "${PWD}/bin:/output" \
+	-w /usr/src/app \
+	"ezhttp:temp-arm64v8" \
+	cp "/usr/src/app/ezhttp.alpine.bin" /output/ezhttp.alpine-arm64v8.bin && \
+	podman image rm "ezhttp:temp-arm64v8";
